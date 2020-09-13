13a2ecea039105dc7137fe2f07ea47232704b315
Anastasiya <soboleva1494@yandex.ru>
2020-09-14 02:53:07 +0300

    fetch

--- src/App.js
+++ src/App.js
@@ -22,9 +22,7 @@ class App extends React.Component {
     /*делаем запрос*/
     const response = await fetch(url, {
       method: "GET",
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      },
+     
     });
     const data = await response.json();
     this.setState({
