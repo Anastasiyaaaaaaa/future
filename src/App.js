fbd8493c0c05459b7d2f965238ae03173584c3ff
Anastasiya <soboleva1494@yandex.ru>
2020-09-14 11:26:56 +0300

    Revert "fetch"
    This reverts commit eb6e294dbfde8920d5e3c9a4a5ddb3743f5778b2.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,10 @@ class App extends React.Component {
 
   async fetchData(url) {
     /*делаем запрос*/
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      method: "GET",
+     
+    });
     const data = await response.json();
     this.setState({
       isLoading: false,
